d233022b61b038f8f46cef403d0445dab7a2ca67
tunishq <74109308+tunishq@users.noreply.github.com>
2022-02-06 10:49:08 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+img = "";
+STATUS= "";
+objects = [];
+
+function preload()
+{
+    img=loadImage("dog_cat.jpg");
+}
+
+function setup ()
+{
+canvas = createCanvas(640, 420);
+canvas.center();
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+
+function draw ()
+{
+    image(img, 0, 0, 640, 420);
+
+    if(STATUS != "")
+    {
+        for(i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : object detected";
+
+            fill("#ff0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#ff0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+        }
+    }
+
+
+}
+
+function modelLoaded ()
+{
+    console.log("model loaded");
+    STATUS = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.error("error");
+    }
+    console.log(results);
+    objects = results;
+}
